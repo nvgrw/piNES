c85e2b69db7eca213a54fe4cfc8ced948b3bb3bb
Nik Vangerow <n@vangerow.uk>
2017-06-03 15:27:57 +0100

    NES binary test reader

--- nes/src/nes.c
+++ nes/src/nes.c
@@ -4,7 +4,23 @@
 #include "cpu.h"
 
 int main(int argc, char** argv) {
+  if (argc != 2) {
+    fprintf(stderr, "Please provide 1 argument. You provided %d.", argc - 1);
+  }
+
   cpu* cpu = cpu_init();
+
+  /* Read image into memory */
+  FILE* fp = fopen(argv[1], "r");
+  fseek(fp, 0, SEEK_END);
+  size_t size = ftell(fp);
+  fseek(fp, 0, SEEK_SET);
+  fread(cpu->memory, 1, size, fp);
+  fclose(fp);
+  /* - */
+
+  cpu->program_counter = 0x400;
+
   cpu_run(cpu);
   cpu_deinit(cpu);
   return EXIT_SUCCESS;
