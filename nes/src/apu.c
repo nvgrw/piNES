2e91899def1da7bc7418dacb2061f55b5d046e56
Andrei Victor Serbanescu <andrei.serbanescu16@imperial.ac.uk>
2017-06-03 18:52:35 +0100

    got rid of magic numbers in apu_mem_is_valid, put hex values for addresses

--- nes/src/apu.c
+++ nes/src/apu.c
@@ -1,10 +1,16 @@
 #include "apu.h"
 #include <stdio.h>
 
+#define START_CHANNELS 0x4000;
+#define END_CHANNELS 0x4013;
+#define STATUS 0x4015;
+#define FRAME_COUNTER 0x4017;
+
 bool apu_mem_is_valid(uint16_t address) {
   uint16_t translated_address = apu_mem_translate(address);
-  return (4000 <= translated_address && translated_address <= 4013) ||
-         translated_address == 4015 || translated_address == 4017;
+  return (START_CHANNELS <= translated_address &&
+          translated_address <= END_CHANNELS) ||
+         translated_address == STATUS || translated_address == FRAME_COUNTER;
 }
 
 uint16_t apu_mem_translate(uint16_t address) { return -1; }
