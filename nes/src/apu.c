e97536799e1539a6c6b539cb795402dfbe31c35a
Andrei Victor Serbanescu <andrei.serbanescu16@imperial.ac.uk>
2017-06-03 18:43:48 +0100

    checked if memory is valid, added a dummy translate method

--- nes/src/apu.c
+++ nes/src/apu.c
@@ -1,2 +1,10 @@
 #include "apu.h"
 #include <stdio.h>
+
+bool apu_mem_is_valid(uint16_t address) {
+  uint16_t translated_address = apu_mem_translate(address);
+  return (4000 <= translated_address && translated_address <= 4013) ||
+         translated_address == 4015 || translated_address == 4017;
+}
+
+uint16_t apu_mem_translate(uint16_t address) { return -1; }
