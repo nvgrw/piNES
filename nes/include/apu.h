07c6f7eaf3b86021b4366325b3cf6f1e29c21567
Andrei Victor Serbanescu <andrei.serbanescu16@imperial.ac.uk>
2017-06-03 18:23:50 +0100

    added pulse 1 & 2, triangle, noise, dmc channels, and control

--- nes/include/apu.h
+++ nes/include/apu.h
@@ -3,3 +3,160 @@
 #include <stdbool.h>
 #include <stdint.h>
 #include <stdlib.h>
+
+
+/* using dummy memory for now
+until the cpu is done */
+
+/*
+typedef struct {
+  uint8_t duty_cycle : 2;
+  uint8_t volume : 4;
+  uint8_t raw_low;
+  uint8_t raw_high : 3;
+} pulse;
+*/
+
+/* NES is little endian */
+typedef union {
+  struct {
+    struct {
+      uint8_t envelope_period_or_volume : 4;
+      uint8_t constant_volume : 1;
+      uint8_t loop_or_disable_length_counter : 1;
+      uint8_t duty_cycle : 2;
+    } reg1;
+
+    /* Sweep unit */
+    struct {
+      uint8_t shift_count : 3;
+      uint8_t negative : 1;
+      uint8_t period : 3;
+      uint8_t enabled : 1;
+    } reg2;
+
+    struct {
+      uint8_t timer_low;
+    } reg3;
+
+    struct {
+      uint8_t high : 3;
+      uint8_t length_counter_load : 5;
+      /* also resets duty and starts envelope_period_or_volume..
+          apparently */
+    } reg4;
+  } regs;
+
+  uint8_t regs_raw[4];
+} pulse_channel;
+
+typedef union {
+
+  struct {
+    struct {
+      uint8_t lin_count_reload_val : 7;
+      uint8_t len_count_disable : 1;
+    } reg1;
+
+    struct {
+      uint8_t timer_low;
+    } reg2;
+
+    struct {
+      uint8_t timer_high : 3;
+      uint8_t len_count_load : 5;
+    } reg3;
+
+  } regs;
+
+  uint8_t regs_raw[3];
+} triangle_channel;
+
+typedef union {
+  struct {
+    struct {
+      uint8_t env_per_or_volume : 4;
+      uint8_t const_volume : 1;
+      uint8_t loop_env_or_dis_len : 1;
+      uint8_t waste : 2;
+    } reg1;
+
+    struct {
+      uint8_t noise_period : 2;
+      uint8_t waste : 3;
+      uint8_t loop_noise : 1;
+    } reg2;
+
+    struct {
+      uint8_t waste : 3;
+      uint8_t lenn_count_load : 5;
+    } reg3;
+  } regs;
+
+  uint8_t regs_raw[3];
+} noise_channel;
+
+typedef union {
+  struct {
+    struct {
+      uint8_t freq_index : 4;
+      uint8_t waste : 2;
+      uint8_t loop_sample : 1;
+      uint8_t irq_enable : 1;
+    } reg1;
+
+    struct {
+      uint8_t direct_load : 7;
+      uint8_t waste : 1;
+    } reg2;
+
+    struct {
+      uint8_t sample_address;
+    } reg3;
+
+  } regs;
+  uint8_t regs_raw[3];
+} dmc_channel;
+
+typedef union {
+  struct {
+    struct {
+      uint8_t pulse1 : 1;
+      uint8_t pulse2 : 1;
+      uint8_t triangle : 1;
+      uint8_t noise : 1;
+    } len_count_enable;
+
+    uint8_t dmc_enable : 1;
+    uint8_t waste : 3;
+  } fields;
+  uint8_t raw;
+} control;
+
+typedef struct {
+  pulse_channel pulse[2];
+  triangle_channel triangle;
+  noise_channel noise;
+  dmc_channel dmc;
+} apu;
+
+/**
+ * Initialise the APU struct, setting all fields
+ * to their default values
+ */
+apu* apu_init(void);
+
+
+/**
+ * Memory acces utility functions
+ */
+
+bool apu_mem_is_valid(uint16_t address);
+bool apu_mem_read8(apu* apu, uint16_t address);
+void apu_mem_read16(apu* apu, uint16_t address);
+
+/**
+ * Register access utility functions
+ */
+
+ void apu_reg_read(apu* apu, uint8_t num);
